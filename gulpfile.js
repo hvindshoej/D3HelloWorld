ac8018b0bb58c04fbd438461dc977399e6ebb63b
hvindshoej <henrik@vindshoej.dk>
2019-12-30 10:28:36 +0100

    Reorganized files and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,70 @@
+'use strict';
+
+const MoveJavaScriptTask = "js";
+const MoveHtmlTask = "html";
+const MoveSassTask = "sass";
+const serveTask = "serve";
+
+const htmlSource = "src/*.html";
+const javaScriptSource = "src/js/*.js";
+const scssSource = "src/scss/*.scss";
+
 var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
-var d3 = require("d3");
+var d3 = require("d3")
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function() {
+gulp.task(MoveSassTask, function() {
   return gulp
-    .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
+    .src([
+      "node_modules/bootstrap/scss/bootstrap.scss", 
+      scssSource])
     .pipe(sass())
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(browserSync.stream());
 });
 
-// Move the javascript files into our /src/js folder
-gulp.task("js", function() {
+// Move the javascript files
+gulp.task(MoveJavaScriptTask, function() {
   return gulp
     .src([
       "node_modules/bootstrap/dist/js/bootstrap.min.js",
       "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/popper.js/dist/umd/popper.min.js"
+      "node_modules/popper.js/dist/umd/popper.min.js",
+      "node_modules/d3/d3.min.js",
+      javaScriptSource
     ])
-    .pipe(gulp.dest("src/js/"))
+    .pipe(gulp.dest("build/js/"))
+    .pipe(browserSync.stream());
+});
+
+// Move the html files
+gulp.task(MoveHtmlTask, function() {
+  return gulp
+    .src([
+      htmlSource
+    ])
+    .pipe(gulp.dest("build"))
     .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
 gulp.task(
-  "serve",
-  gulp.series(["sass"], function() {
+  serveTask,
+  gulp.series(
+    [MoveSassTask], function() {
     browserSync.init({
-      server: "./src"
+      server: "./build"
     });
 
-    gulp.watch(
-      ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],gulp.series(["sass"])
-    );
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch([scssSource], gulp.series([MoveSassTask]));
+    gulp.watch([javaScriptSource], gulp.series([MoveJavaScriptTask]));
+    gulp.watch([htmlSource], gulp.series([MoveHtmlTask]));
+
+    gulp.watch([scssSource, javaScriptSource, htmlSource])
+      .on("change", browserSync.reload);
   })
 );
 
-gulp.task("default", gulp.series(["js", "serve"]));
+gulp.task("default", gulp.series([MoveJavaScriptTask, MoveHtmlTask, serveTask]));
---
+++ src/js/main.js
@@ -0,0 +1,144 @@
+"use strict";
+
+const rectWidth = 350;
+const lineHeight = 24;
+const textPadding = 12;
+
+var svg = d3.select("body")
+    .append("svg")
+        .attr("id", "svg")
+        .attr("width", "100vw")
+        .attr("min-width", "100vw")
+        .attr("height", "100vh")
+        .attr("min-height", "100vh");
+
+var width = document.getElementById('svg').clientWidth;
+var height = document.getElementById('svg').clientHeight;
+
+var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(200)
+    .size([width, height]);
+
+var mis = document.getElementById('mis').innerHTML;
+var graph = JSON.parse(mis);
+
+force.nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+
+var node_drag = d3.behavior.drag()
+    .on("dragstart", dragstart)
+    .on("drag", dragmove)
+    .on("dragend", dragend);
+
+    var links = svg.selectAll(".link")
+    .data(graph.links)
+    .enter()
+    .append("line")
+        .attr("class", "link")
+        .style("stroke", "rgb(0,0,0)")
+        .style("stroke-width", "1");
+
+var nodes = svg.selectAll(".node")
+    .data(graph.nodes)
+    .enter()
+    .append("g")
+    .append("svg")
+    .on('dblclick', releasenode)
+    .call(node_drag);
+
+var texts = nodes.selectAll("text")
+    .data(d => d.attributes)
+    .enter()
+    .append("text")
+        .attr("class", "keyvalue")
+        .attr("y", (d, i) =>  lineHeight + i * lineHeight)
+        .attr("x", textPadding)
+        .text(d => d.key + ": " + d.value);
+
+var rects = nodes.selectAll("rect")
+    .data(d => function(d) { return d; })
+    .enter()
+    .append("rect")
+        .attr("width", rectWidth)
+        .attr("height",
+            function() 
+            { 
+                var numberOfTextElements = this.parentElement.getElementsByClassName("keyvalue").length;
+                return rectangleHeight(numberOfTextElements); 
+            })
+        .style("stroke", "rgb(0,0,255)")
+        .attr("opacity", "10%");
+
+force.on(
+    "tick", 
+    function () 
+    {
+        links
+            .attr("x1", function (d) { return d.source.x + rectWidth / 2; })
+            .attr("y1", function (d) { return d.source.y + rectangleHeight(d.source.attributes.length) / 2 })
+            .attr("x2", function (d) { return d.target.x + rectWidth / 2; })
+            .attr("y2", function (d) { return d.target.y + rectangleHeight(d.target.attributes.length) / 2 })
+
+        nodes
+            .attr("x", function (d) { return d.x; })
+            .attr("y", function (d) { return d.y; });
+
+        nodes.each(collide(0.5));
+    });
+
+function dragstart(d, i) {
+    force.stop() // stops the force auto positioning before you start dragging
+}
+
+function dragmove(d, i) {
+    d.px += d3.event.dx;
+    d.py += d3.event.dy;
+    d.x += d3.event.dx;
+    d.y += d3.event.dy;
+}
+
+function dragend(d, i) {
+    d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+    force.resume();
+}
+
+function releasenode(d) {
+    d.fixed = false; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+    force.resume();
+}        
+
+function rectangleHeight(numberOfTextElements)
+{
+    return numberOfTextElements * lineHeight + textPadding;
+}
+
+function collide(alpha) {
+    var padding = 100,
+    radius = 80;
+
+    var quadtree = d3.geom.quadtree(graph.nodes);
+    return function (d) {
+        var rb = 2 * radius + padding,
+            nx1 = d.x - rb,
+            nx2 = d.x + rb,
+            ny1 = d.y - rb,
+            ny2 = d.y + rb;
+        quadtree.visit(function (quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== d)) {
+                var x = d.x - quad.point.x,
+                    y = d.y - quad.point.y,
+                    l = Math.sqrt(x * x + y * y);
+                if (l < rb) {
+                    l = (l - rb) / l * alpha;
+                    d.x -= x *= l;
+                    d.y -= y *= l;
+                    quad.point.x += x;
+                    quad.point.y += y;
+                }
+            }
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        });
+    };
+}
