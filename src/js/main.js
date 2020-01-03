7468536d56e7c5745bf4d7beb78fc1403f58bcf9
hvindshoej <henrik@vindshoej.dk>
2020-01-03 08:55:19 +0100

    Use id as key in links instead of index-based key

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const rectWidth = 350;
-const lineHeight = 24;
+const lineHeight = 20;
 const textPadding = 12;
 
 var svg = d3.select("svg");
@@ -12,9 +12,9 @@ var links = [];
 var width = document.getElementById('svg').clientWidth;
 var height = document.getElementById('svg').clientHeight;
 
-var simulation = d3.forceSimulation(nodes)
+var simulation = d3.forceSimulation()
     .force("charge", d3.forceManyBody().strength(-500))
-    .force("link", d3.forceLink(links).distance(500))
+    .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(400))
     .force("x", d3.forceX())
     .force("y", d3.forceY())
     .alphaTarget(1)
@@ -39,17 +39,15 @@ var node = g
 function LoadJson(jsonString)
 {
     var graph = JSON.parse(jsonString);
-    links = graph.links;
-    nodes = graph.nodes;
 
-    link = link.data(links);
+    link = link.data(graph.links);
     link.exit().remove();
     link = link
         .enter()
         .append("line")
         .merge(link);;
 
-    node = node.data(nodes, function(d) { return d.id;} );
+    node = node.data(graph.nodes, function(d) { return d.id;} );
     node.exit().remove();
     node = node
         .enter()
@@ -87,9 +85,8 @@ function LoadJson(jsonString)
             .style("stroke", "rgb(0,0,255)")
             .attr("opacity", "10%");
 
-    simulation.nodes(nodes);
-    simulation.force("link").links(links);
-    simulation.alpha(1).restart();
+    simulation.nodes(graph.nodes);
+    simulation.force("link").links(graph.links);
 }
 
 function rectangleHeight(numberOfTextElements)
@@ -97,64 +94,15 @@ function rectangleHeight(numberOfTextElements)
     return numberOfTextElements * lineHeight + textPadding;
 }
 
-function collide(alpha) {
-    var padding = 100,
-    radius = 80;
-
-    var quadtree = d3.geom.quadtree(graph.nodes);
-    return function (d) {
-        var rb = 2 * radius + padding,
-            nx1 = d.x - rb,
-            nx2 = d.x + rb,
-            ny1 = d.y - rb,
-            ny2 = d.y + rb;
-        quadtree.visit(function (quad, x1, y1, x2, y2) {
-            if (quad.point && (quad.point !== d)) {
-                var x = d.x - quad.point.x,
-                    y = d.y - quad.point.y,
-                    l = Math.sqrt(x * x + y * y);
-                if (l < rb) {
-                    l = (l - rb) / l * alpha;
-                    d.x -= x *= l;
-                    d.y -= y *= l;
-                    quad.point.x += x;
-                    quad.point.y += y;
-                }
-            }
-            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-        });
-    };
-}
-
 function ticked() 
 {
     node
-        .attr("x", function(d) 
-        { 
-            //console.log(d);
-            return d.x; 
-        })
-        .attr("y", function(d) 
-        { 
-            //return 0;
-            return d.y; 
-        });
+        .attr("x", d => d.x)
+        .attr("y", d => d.y);
 
     link
-        .attr("x1", function (d) 
-        {
-            return d.source.x + rectWidth / 2; 
-        })
-        .attr("y1", function (d) 
-        {
-            return d.source.y + rectangleHeight(d.source.attributes.length) / 2;
-        })
-        .attr("x2", function (d) 
-        {
-             return d.target.x + rectWidth / 2; 
-        })
-        .attr("y2", function (d) 
-        { 
-            return d.target.y + rectangleHeight(d.target.attributes.length) / 2;
-        });
+        .attr("x1", d => d.source.x + rectWidth / 2)
+        .attr("y1", d => d.source.y + rectangleHeight(d.source.attributes.length) / 2)
+        .attr("x2", d => d.target.x + rectWidth / 2)
+        .attr("y2", d => d.target.y + rectangleHeight(d.target.attributes.length) / 2);
 }
